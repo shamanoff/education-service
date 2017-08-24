28a436ad51ec2ca0b5936af223af3328f884b07b
shamanoff <shamanoff@gmail.com>
2017-08-24 21:12:06 +0300

    home

--- src/app/question-input/question.service.ts
+++ src/app/question-input/question.service.ts
@@ -5,15 +5,60 @@ import {Question} from './question';
 @Injectable()
 export class QuestionService {
   public question$: FirebaseListObservable<Question[]>;
+  questionsIds: Array<string> = [];
 
   constructor( private _db: AngularFireDatabase) {
   }
 
-  getQuestions() {
+  getQuestions(tag: string = null) {
+    if (tag != null) {
+      this.question$ = this._db.list('/questions', {
+        query: {
+          // limitToLast: (10),
+          //
+          orderByChild: 'tag',
+          equalTo: tag,
+          preserveSnapshot: true
+        }
+      });
+    } else {
+      this.question$ = this._db.list('/questions', {
+        query: {
+          // limitToLast: (10),
+
+          // orderByChild: 'tag',
+          // equalTo: tag
+        }
+      }) as
+        FirebaseListObservable<Question[]>;
+    }
+
+    return this.question$;
+  }
+
+  getIdsArray(tag): any {
+    this._db.list('/questions', {
+      preserveSnapshot: true,
+      query: {
+        orderByChild: 'tag',
+        equalTo: tag,
+
+      },
+    }).subscribe(snapshots => {
+      snapshots.forEach(snapshot => {
+        this.questionsIds.push(snapshot.value);
+        console.log(snapshot.key);
+      });
+
+      return this.questionsIds;
+    });
+  }
+
+ /* getQuestions() {
     this.question$ = this._db.list('/questions') as
       FirebaseListObservable<Question[]>;
     return this.question$;
-  }
+  }*/
 
   addQuestion(data) {
     return this.question$.push(data);
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,18 +6,18 @@ import {QuestionService} from '../question-input/question.service';
 import {FullExam} from '../exzam-chooser/fullExam';
 import {ExamService} from '../exzam-chooser/exam.service';
 import {Response} from '@angular/http';
-import {ExamSection} from "../exzam-chooser/examSection";
-import * as _ from "lodash";
+import {ExamSection} from '../exzam-chooser/examSection';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent implements OnInit{
+export class TestComponent implements OnInit {
 
   curExam: FirebaseListObservable<ExamSection[]>;
-
+public idAr: string[]=[];
   question$: FirebaseListObservable<Question[]>;
   // curExam: FirebaseObjectObservable<FullExam>;
   httpExam: FullExam;
@@ -32,38 +32,40 @@ export class TestComponent implements OnInit{
     this.question$ = this._qS.getQuestions();
     console.log(this.key + ' KEY');
     this.getCurExamByKey(this.key);
+    this.idAr = this._qS.getIdsArray('Java');
+    console.log(this.idAr)
     // this.curExam = this._eS.getExamByKey(this.key);
     // this.httpExam = this._eS.getExamByKey(this.key);
     // this.onGet(this.key);
 
   }
 
-  exGen(){
+  exGen() {
     this.curExam.subscribe(
-      (v)=>{
-        console.log(v)
+      (v) => {
+        console.log(v);
       }
     );
   }
 
-  getCurExamByKey(key){
+  getCurExamByKey(key) {
 
     this.curExam = this._eS.getExamByKey(key);
   }
 
 
-/*
-  onGet(key) {
-    this._eS.getExamByKey(key)
-      .subscribe(
-        (response: Response) => {
-          this.httpExam = response.json();
+  /*
+    onGet(key) {
+      this._eS.getExamByKey(key)
+        .subscribe(
+          (response: Response) => {
+            this.httpExam = response.json();
 
-          console.log(this.httpExam);
-        }, (error) => console.log(error)
-      );
-  }
-*/
+            console.log(this.httpExam);
+          }, (error) => console.log(error)
+        );
+    }
+  */
 
   ngOnInit() {
   }
